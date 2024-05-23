3f9960d9b0ed47534788680b1fb844a072b8149e
Cakeblade <jch1070@gmail.com>
2024-05-23 22:58:10 +0900

    docs : comment out unused variables

--- crawller.py
+++ crawller.py
@@ -14,9 +14,9 @@ load_dotenv()
 GIHU_TABLE = os.environ.get('GIHU_TABLE')
 
 # no need in ec2
-AWS_REGION = os.environ.get('AWS_REGION')
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+# AWS_REGION = os.environ.get('AWS_REGION')
+# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 # initialize db
 client = boto3.client(
