e6342f9018a0e5d8f4383a06f6c45c6aab78d973
Ubuntu <ubuntu@ip-172-31-26-175.ap-southeast-1.compute.internal>
2024-05-23 04:54:00 +0000

    feat : surpport ec2

--- crawller.py
+++ crawller.py
@@ -1,23 +1,25 @@
-import pandas as pd
 import os
-import csv
+import sys
 import boto3
 
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 
-# aws key
+# table
 load_dotenv()
-
-AWS_REGION = os.environ.get('AWS_REGION')
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 GIHU_TABLE = os.environ.get('GIHU_TABLE')
 
+# make webdriver options (ubuntu setting)
+option = Options()
+option.add_argument("--headless")
+option.add_argument('--no-sandbox')
+option.add_argument('--disable-dev-shm-usage')
+
 # load webdriver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=option)
 url = 'https://func.seoul.go.kr/climateCard/rangeList.do'
 table_data = []
 items = []
@@ -46,49 +48,54 @@ for i in range(1, 36):
 # driver quit
 driver.quit()
 
+if len(table_data) > 0:
+    print("Crawling Complete")
+else:
+    sys.exit()
 
-# initialize db
+# # initialize db
 db = boto3.resource(
-    'dynamodb',
-    region_name=AWS_REGION,
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+    'dynamodb'
 )
 
-# gihu_table = db.create_table(
-#     TableName=GIHU_TABLE,
-#     KeySchema=[
-#         {   
-#             'AttributeName': 'name',
-#             'KeyType': 'HASH'
-#         },
-#         {
-#             'AttributeName': 'index',
-#             'KeyType': 'RANGE'
-#         }
-#     ],
-#     AttributeDefinitions=[
-#         {
-#             'AttributeName': 'name',
-#             'AttributeType': 'S'
-#         },
-#         {
-#             'AttributeName': 'index',
-#             'AttributeType': 'N'
-#         }
-#     ],
-#     ProvisionedThroughput={
-#         'ReadCapacityUnits': 10,
-#         'WriteCapacityUnits': 10
-#     }
-# )
+db_table_name = [table.name for table in db.tables.all()]
+
+if GIHU_TABLE not in db_table_name:
+    gihu_table = db.create_table(
+        TableName=GIHU_TABLE,
+        KeySchema=[
+            {   
+                'AttributeName': 'name',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'id',
+                'KeyType': 'RANGE'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'name',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'N'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+    )
 
 gihu_table = db.Table(GIHU_TABLE)
 
+# insert data
 for row in table_data:    
     gihu_table.put_item(
         Item = {
-            'index' : int(row[0]),
+            'id' : int(row[0]),
             'name' : row[1],
             'line' : row[2],
             'riding' : True if row[3] == 'O' else False,
